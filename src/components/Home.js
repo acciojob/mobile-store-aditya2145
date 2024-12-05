4b6a367bae7d6b24b8fa4972669b1c227f830ee6
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-12-05 12:05:39 +0530

    Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+
+import React, { useContext } from 'react'
+import DataContext from '../context/DataContext'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  const { data } = useContext(DataContext)
+  return (
+    <div>Home
+      {data.map((el, i) => {
+        return (
+          <div key={el.id} style={{ border: "1px solid black" }} >
+            <h3>{el.name}</h3>
+            <p>{el.desc}</p>
+            <b>{el.price}</b>
+            <img height="100px" src={el.image} alt="" />
+            <Link to={`/products/${el.id}`}><button className='btn' >Buy</button></Link>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Home
